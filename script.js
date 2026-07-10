4a46f68290f10f3173a0e0038c25039a241ee1b4
zurouni <fadilyas45@gmail.com>
2026-07-10 23:00:40 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const header = document.getElementById("header");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 80) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-});

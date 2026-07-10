39bc45b2b0cba42372bc98d9f0ab2ffb8e6a0283
zurouni <fadilyas45@gmail.com>
2026-07-10 22:54:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});

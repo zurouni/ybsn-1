20cf08f425bdcb76bf5ed2f424fa0bc04fa4db0d
zurouni <fadilyas45@gmail.com>
2026-07-12 23:57:14 +0700

    Improve header scroll effects and footer slogan behavior
    Enhanced header behavior on scroll and footer slogan positioning.

--- script/script.js
+++ script/script.js
@@ -1,9 +1,66 @@
+/* =====================================
+   HEADER
+===================================== */
+
 const header = document.getElementById("header");
+const logo = document.getElementById("logo-img");
+
+let headerScrolled = false;
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 80) {
+  const scrollTop = window.scrollY;
+
+  /* =====================
+       SCROLLED
+    ===================== */
+
+  if (scrollTop > 80 && !headerScrolled) {
+    headerScrolled = true;
+
     header.classList.add("scrolled");
-  } else {
+
+    logo.style.opacity = "0";
+
+    setTimeout(() => {
+      logo.src = "img/logo-black.png";
+
+      logo.style.opacity = "1";
+    }, 150);
+  } else if (scrollTop <= 80 && headerScrolled) {
+    /* =====================
+       TOP
+    ===================== */
+    headerScrolled = false;
+
     header.classList.remove("scrolled");
+
+    logo.style.opacity = "0";
+
+    setTimeout(() => {
+      logo.src = "img/logo-white.png";
+
+      logo.style.opacity = "1";
+    }, 150);
+  }
+});
+
+// Footer Slogan
+
+const slogan = document.querySelector(".sticky-slogan");
+const footer = document.querySelector("footer");
+
+window.addEventListener("scroll", () => {
+  const footerTop = footer.getBoundingClientRect().top;
+
+  if (footerTop < window.innerHeight) {
+    slogan.style.position = "absolute";
+
+    slogan.style.top = window.scrollY + footerTop - 60 + "px";
+  } else {
+    slogan.style.position = "fixed";
+
+    slogan.style.top = "";
+
+    slogan.style.bottom = "0";
   }
 });
